e846d1d0a6166a40e26f048f0f8b47ecdc6d67e7
Nate5461 <nbissett@uwo.ca>
2023-12-01 15:52:41 -0500

    Got backend working with database

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+      <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* Other routes go here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
@@ -18,6 +26,37 @@ function Header() {
     </header>
   );
 }
+
+function Login() {
+  // Replace with your own login form
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+function Signup() {
+  // Replace with your own signup form
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form>
+        <input id="makeUser" type="text" placeholder="UserName" />
+        <input id= "makeEmail" type="text" placeholder="Email" />
+        <input id="makePass" type="password" placeholder="Password" />
+        <button type="submit">Signup</button>
+      </form> 
+    </div>
+  );
+}
+
+
 
 function SearchArea() {
   return (
@@ -36,10 +75,11 @@ function SearchArea() {
 
 function Main() {
   return (
-    <main>
-      <ListActions />
-      <SplitContainer />
-    </main>
+    <div id="splashPage">
+      <h1>Welcome to Nate's Superheros</h1>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,126 +1,135 @@
 const express = require('express');
-const fs = require('fs'); 
-const path = require('path');
-const rateLimit = require('express-rate-limit');
-
-const listsFilePath = './superhero_lists.json'
-const { check, validationResult } = require('express-validator');
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100000 // limit each IP to 1000 requests per windowMs
-});
-
 const app = express();
-const port = 3000;
 
-app.use(limiter);
+const { MongoClient } = require('mongodb');
 
-app.use('/', express.static('../client/public/index.html'));
+const client = new MongoClient('mongodb://127.0.0.1:27017');
 
-//More middleware for json parsing
-app.use(express.json());
+async function startServer() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
 
-app.use(express.json({ limit: '1mb' })); 
+        const superheroInfoCollection = client.db('mydb').collection('superheroInfo');
+        const superheroPowersCollection = client.db('mydb').collection('superheroPowers');
+
+        app.get('/api/superhero_info', async (req, res) => {
+            console.log('Fetching superhero info...');
+            try {
+                const docs = await superheroInfoCollection.find().toArray();
+                res.json(docs);
+            } catch (err) {
+                console.error('Error fetching superhero info:', err);
+                res.status(500).send(err);
+            }
+        });
+
+        app.get('/api/superhero_powers', async (req, res) => {
+            console.log('Fetching superhero powers...');
+            try {
+                const docs = await superheroPowersCollection.find().toArray();
+                console.log('Fetched superhero powers:', docs);
+                res.json(docs);
+            } catch (err) {
+                console.error('Error fetching superhero powers:', err);
+                res.status(500).send(err);
+            }
+        });
+
+        //Get all superhero information
+        /*
+        app.get('/api/superhero_info/:id', async (req, res) => {
+            const heroId = parseInt(req.params.id, 10); // Parse the id to an integer, if necessary
+            try {
+                const hero = await superheroInfoCollection.findOne({ id: heroId }); // Find the hero with the matching ID
+                if (hero) {
+                    res.json(hero); // Send the hero data
+                } else {
+                    res.status(404).send(`Hero with ID ${heroId} not found.`); // Send a 404 if not found
+                }
+            } catch (err) {
+                console.error(err);
+                res.status(500).send('An error occurred while fetching the hero data.');
+            }
+        });
+        */
+
+        app.get('/api/superhero_info/:id', async (req, res) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+        
+            const heroId = parseInt(req.params.id, 10); // Parse the id to an integer, if necessary
+        
+            try {
+                const hero = await superheroInfoCollection.findOne({ id: heroId }); // Find the hero with the matching ID
+                if (hero) {
+                    const heroPowersEntry = await superheroPowersCollection.findOne({ hero_names: hero.name });
+                    let heroPowers = {};
+        
+                    if (heroPowersEntry) {
+                        heroPowers = Object.entries(heroPowersEntry)
+                            .reduce((obj, [key, value]) => {
+                                if (value === "True") {
+                                    obj[key] = true;
+                                }
+                                return obj;
+                            }, {});
+                    }
+        
+                    res.json({ ...hero, powers: heroPowers }); // Send the hero's info and powers
+                } else {
+                    res.status(404).send(`Hero with ID ${heroId} not found.`); // Send a 404 if not found
+                }
+            } catch (err) {
+                console.error(err);
+                res.status(500).send('An error occurred while fetching the hero data.');
+            }
+        });
+
+        const port = 3000;
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+        app.use(express.static('../client/build'));
+
+        const rateLimit = require('express-rate-limit');
 
 
-//Set up middleware
-app.use((req, res, next) => {
-    console.log(`${req.method} request for ${req.url}`);
-    next();
-})
+        app.use(express.json({ limit: '1mb' })); 
+
+        const { check, validationResult } = require('express-validator');
+
+        const limiter = rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100000 // limit each IP to 1000 requests per windowMs
+        });
+
+        //Set up middleware
+        app.use((req, res, next) => {
+            console.log(`${req.method} request for ${req.url}`);
+            next();
+        })
 
 
+        //app.use(limiter);
+        } catch (err) {
+            console.error('Could not connect to MongoDB', err);
+        }
+}
 
-// Send JSON file for the first route
-app.get('/api/superhero_info', (req, res) => {
-    fs.readFile('./superhero_info.json', 'utf8', (err, data) => {
-        res.json(JSON.parse(data));
-    });
-});
+startServer();
 
-// Send JSON file for the second route
-app.get('/api/superhero_powers', (req, res) => {
-    fs.readFile('./superhero_powers.json', 'utf8', (err, data) => {
-        res.json(JSON.parse(data));
-    });
-});
 
 
 /*
-//Get all superhero information
-app.get('/api/superhero_info/:id', (req, res) => {
-    const heroId = parseInt(req.params.id, 10); // Parse the id to an integer, if necessary
-    fs.readFile('./superhero_info.json', 'utf8', (err, data) => {
-        try {
-            const heroes = JSON.parse(data); // Parse the JSON data
-            const hero = heroes.find(h => h.id === heroId); // Find the hero with the matching ID
-            if (hero) {
-                res.json(hero); // Send only the hero's name
-            } else {
-                res.status(404).send(`Hero with ID ${heroId} not found.`); // Send a 404 if not found
-            }
-        } catch (parseError) {
-            console.error(parseError);
-            res.status(500).send('An error occurred while parsing the JSON data.');
-        }in
-    });
-});
-*/
 
-app.get('/api/superhero_info/:id', (req, res) => {
 
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.arrary() });
-    }
 
-    const heroId = parseInt(req.params.id, 10); // Parse the id to an integer, if necessary
-
-    fs.readFile('./superhero_info.json', 'utf8', (err, data) => {
-        try {
-            const heroes = JSON.parse(data); // Parse the JSON data
-            const hero = heroes.find(h => h.id === heroId); // Find the hero with the matching ID
-
-            if (hero) {
-                // Read the superhero powers file
-                fs.readFile('./superhero_powers.json', 'utf8', (powersErr, powersData) => {
-                    if (powersErr) {
-                        console.error(powersErr);
-                        return res.status(500).send('An error occurred while reading the powers JSON file.');
-                    }
-
-                    try {
-                        const powersList = JSON.parse(powersData);
-                        const heroPowersEntry = powersList.find(p => p.hero_names === hero.name);
-                        let heroPowers = {};
-
-                        if (heroPowersEntry) {
-                            heroPowers = Object.entries(heroPowersEntry)
-                                .reduce((obj, [key, value]) => {
-                                    if (value === "True") {
-                                        obj[key] = true;
-                                    }
-                                    return obj;
-                                }, {});
-                        }
-
-                        res.json({ ...hero, powers: heroPowers }); // Send the hero's info and powers
-                    } catch (parseError) {
-                        console.error(parseError);
-                        res.status(500).send('An error occurred while parsing the powers JSON data.');
-                    }
-                });
-            } else {
-                res.status(404).send(`Hero with ID ${heroId} not found.`); // Send a 404 if not found
-            }
-        } catch (parseError) {
-            console.error(parseError);
-            res.status(500).send('An error occurred while parsing the JSON data.');
-        }
-    });
-});
 
 // Get all hero names that have a superpower matching the search term (partial matches included)
 app.get('/api/superhero_powers/:power', (req, res) => {
@@ -238,6 +247,7 @@ app.get('/api/publishers', (req, res) => {
         }
     });
 });
+
 
 //Search for ids with given field/search term/ and number of results
 app.get('/api/superhero_info/:field/:pattern/:n?', (req, res) => {
@@ -620,3 +630,6 @@ app.get('/api/superhero_lists/:listName/details', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+*/
+
+
